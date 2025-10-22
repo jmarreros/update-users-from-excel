bd863e25cc507a56102d7ebacc1593606927a46c
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2025-10-22 11:47:56 -0500

    Agregado cálculo columna observacion7

--- includes/UserRoles.php
+++ includes/UserRoles.php
@@ -10,7 +10,6 @@ class UserRoles {
 	}
 
 
-	// Se puede enviar roles en la columna roles (acepta varios roles separado por comas), observaciones_persona, tipo_abono, tipo_de_socio, observa_11
 	public static function update_custom_roles( $id_user, $item ): string {
 
 		// Validate if the constant DCMS_CUSTOMAREA_ROLES from the custom-area-sporting plugin is defined
@@ -40,10 +39,12 @@ class UserRoles {
 	}
 
 	// Agrega roles desde varios campos al campo de roles y devuelve un array de roles únicos y validados
+	// Se puede enviar roles en la columna roles (acepta varios roles separado por comas), observaciones_persona, tipo_abono, tipo_de_socio, observa_11
+	// Para el observa7 es un % que define el cambio de rol DCMS_CUSTOMAREA_ROLES_LEALTAD, si es >= 90 es nivel_rojiblanco, >= 100 es nivel_dorado y viceversa, si es <90 es nivel_general
 	public static function add_roles_to_item( $item ): array {
 		$roles_arr = [];
 
-		// Procesar el campo principal (puede contener varios roles separados por comas)
+		// Procesar la columna roles ya que puede contener varios roles separados por comas
 		if ( ! empty( $item->roles ) ) {
 			$parts = array_map( 'trim', explode( ',', (string) $item->roles ) );
 			foreach ( $parts as $p ) {
@@ -53,7 +54,7 @@ class UserRoles {
 			}
 		}
 
-		// Campos adicionales a comprobar para agregar a los roles
+		// Campos adicionales para agregar a los roles
 		$extra_fields = [ 'observation_person', 'sub_type', 'soc_type', 'observation11' ];
 		foreach ( $extra_fields as $prop ) {
 			$val = trim( $item->{$prop} ?? '' );
@@ -67,6 +68,31 @@ class UserRoles {
 		$roles_arr = array_map( function ( $role ) {
 			return strtolower( str_replace( ' ', '_', trim( $role ) ) );
 		}, $roles_arr );
+
+
+		// Procesar el campo observa7 para asignar roles de lealtad según el porcentaje
+		$observa7 = trim( $item->observation7 ?? '' );
+
+		if ( $observa7 !== ''){
+			// Puede ser un número o número con %, solo quedarse con el número, ejemplo 100, 100%, 85, 85%
+			$observa7 = rtrim( $observa7, '%' );
+			if ( is_numeric( $observa7 ) ) {
+
+				// Quitar los roles de lealtad si existen en el array
+				$roles_arr = array_filter( $roles_arr, function ( $role ) {
+					return ! in_array( $role, DCMS_CUSTOMAREA_ROLES_LEALTAD );
+				} );
+
+				$observa7 = floatval( $observa7 );
+				if ( $observa7 >= 100 ) {
+					$roles_arr[] = 'nivel_dorado';
+				} elseif ( $observa7 >= 90 ) {
+					$roles_arr[] = 'nivel_rojiblanco';
+				} else {
+					$roles_arr[] = 'nivel_general';
+				}
+			}
+		}
 
 		return array_intersect( $roles_arr, DCMS_CUSTOMAREA_ROLES);
 	}
