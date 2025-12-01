4b2f72185845ceffbdd87df265e4d771e003dba5
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2025-12-01 11:46:36 -0500

    Corrección consulta de sincronización

--- includes/Database.php
+++ includes/Database.php
@@ -36,7 +36,7 @@ class Database {
                 ORDER BY uu.id ASC";
 
 		if ( $limit > 0 ) {
-			$sql .= $this->wpdb->prepare(" LIMIT %d OFFSET %d", $limit, $offset);
+			$sql .= $this->wpdb->prepare( " LIMIT %d OFFSET %d", $limit, $offset );
 		}
 
 		return $this->wpdb->get_results( $sql );
@@ -166,10 +166,10 @@ class Database {
 
 		$table = $this->table_user_data;
 
-		$fields = Helper::get_config_fields();
-		$all_field_keys = array_keys($fields);
-		$default_data = array_fill_keys($all_field_keys, null);
-		$user_data = array_merge($default_data, $user_data);
+		$fields         = Helper::get_config_fields();
+		$all_field_keys = array_keys( $fields );
+		$default_data   = array_fill_keys( $all_field_keys, null );
+		$user_data      = array_merge( $default_data, $user_data );
 
 		// Prepara las columnas y los marcadores de posición para la parte INSERT
 		$columns      = '`' . implode( '`, `', array_keys( $user_data ) ) . '`';
@@ -188,8 +188,12 @@ class Database {
 		$sql = "INSERT INTO `$table` ($columns) VALUES ($placeholders)
 	            ON DUPLICATE KEY UPDATE $update_clause";
 
+
 		// Prepara y ejecuta la consulta
 		$query = $this->wpdb->prepare( $sql, array_values( $user_data ) );
+
+		error_log(print_r('Consulta',true));
+		error_log(print_r($query,true));
 
 		return $this->wpdb->query( $query );
 	}
@@ -202,15 +206,15 @@ class Database {
 	}
 
 	public function delete_user_not_imported(): void {
-		$users_table      = $this->wpdb->users;
-		$usermeta_table   = $this->wpdb->usermeta;
-		$user_data_table  = $this->table_user_data;
-		$tmp_import_table = $this->table_tmp_import;
-		$capabilities_key = $this->wpdb->prefix . 'capabilities';
+		$users_table       = $this->wpdb->users;
+		$usermeta_table    = $this->wpdb->usermeta;
+		$user_data_table   = $this->table_user_data;
+		$tmp_import_table  = $this->table_tmp_import;
+		$capabilities_key  = $this->wpdb->prefix . 'capabilities';
 		$temp_delete_table = 'temp_users_to_delete';
 
 		// 1. Crear una tabla temporal para almacenar los IDs de los usuarios a eliminar.
-		$this->wpdb->query("CREATE TEMPORARY TABLE {$temp_delete_table} (user_id BIGINT(20) NOT NULL, PRIMARY KEY (user_id))");
+		$this->wpdb->query( "CREATE TEMPORARY TABLE {$temp_delete_table} (user_id BIGINT(20) NOT NULL, PRIMARY KEY (user_id))" );
 
 		// 2. Identificar y guardar los IDs de los usuarios que no son administradores y no están en la importación.
 		$sql_select_ids = $this->wpdb->prepare(
@@ -225,16 +229,16 @@ class Database {
 			'%administrator%'
 		);
 
-		$this->wpdb->query($sql_select_ids);
+		$this->wpdb->query( $sql_select_ids );
 
 		// 3. Eliminar los registros de las tablas correspondientes usando la tabla temporal.
 		// Se usa JOIN para un borrado más eficiente.
-		$this->wpdb->query("DELETE ud FROM {$user_data_table} ud JOIN {$temp_delete_table} tmp ON ud.user_id = tmp.user_id");
-		$this->wpdb->query("DELETE um FROM {$usermeta_table} um JOIN {$temp_delete_table} tmp ON um.user_id = tmp.user_id");
-		$this->wpdb->query("DELETE u FROM {$users_table} u JOIN {$temp_delete_table} tmp ON u.ID = tmp.user_id");
+		$this->wpdb->query( "DELETE ud FROM {$user_data_table} ud JOIN {$temp_delete_table} tmp ON ud.user_id = tmp.user_id" );
+		$this->wpdb->query( "DELETE um FROM {$usermeta_table} um JOIN {$temp_delete_table} tmp ON um.user_id = tmp.user_id" );
+		$this->wpdb->query( "DELETE u FROM {$users_table} u JOIN {$temp_delete_table} tmp ON u.ID = tmp.user_id" );
 
 		// 4. Eliminar la tabla temporal.
-		$this->wpdb->query("DROP TEMPORARY TABLE IF EXISTS {$temp_delete_table}");
+		$this->wpdb->query( "DROP TEMPORARY TABLE IF EXISTS {$temp_delete_table}" );
 	}
 
 
@@ -243,45 +247,54 @@ class Database {
 		$this->truncate_table_user_data();
 
 		$sql = "
-				INSERT INTO wp_dcms_user_data
-				SELECT user_id,
-                    GROUP_CONCAT(CASE WHEN meta_key = 'identify' THEN meta_value END) as 'identify',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'pin' THEN meta_value END) as 'pin',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'number' THEN meta_value END) as 'number',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'reference' THEN meta_value END) as 'reference',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'nif' THEN meta_value END) as 'nif',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'first_name' THEN meta_value END) as 'name',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'lastname' THEN meta_value END) as 'lastname',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'birth' THEN meta_value END) as 'birth',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'sub_type' THEN meta_value END) as 'sub_type',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'address' THEN meta_value END) as 'address',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'postal_code' THEN meta_value END) as 'postal_code',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'country' THEN meta_value END) as 'country',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'ayuntamiento' THEN meta_value END) as 'ayuntamiento',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'local' THEN meta_value END) as 'local',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'zone' THEN meta_value END) as 'zone',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'email' THEN meta_value END) as 'email',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'phone' THEN meta_value END) as 'phone',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'mobile' THEN meta_value END) as 'mobile',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'soc_type' THEN meta_value END) as 'soc_type',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'observation7' THEN meta_value END) as 'observation7',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'observation5' THEN meta_value END) as 'observation5',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'observation10' THEN meta_value END) as 'observation10',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'observation11' THEN meta_value END) as 'observation11',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'observation19' THEN meta_value END) as 'observation19',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'sub_permit' THEN meta_value END) as 'sub_permit',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'observation_person' THEN meta_value END) as 'observation_person',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'date_register' THEN meta_value END) as 'date_register',
-                    GROUP_CONCAT(CASE WHEN meta_key = 'roles' THEN meta_value END) as 'roles'
-                FROM
-                    $this->table_meta WHERE
-                    meta_key in ('identify', 'pin', 'number', 'reference', 'nif', 'first_name', 'lastname', 'birth', 'sub_type', 
-                                 'address', 'postal_code', 'local', 'country', 'ayuntamiento', 'email', 'phone', 'mobile', 'zone', 
-                                 'soc_type', 'observation7', 'observation5','observation10','observation11','observation19', 
-                                 'sub_permit', 'observation_person','date_register', 'roles')
-                GROUP BY user_id
-                HAVING MAX(CASE WHEN meta_key = 'number' THEN meta_value END) <> ''";
-
+		    INSERT INTO $this->table_user_data (
+		        user_id, identify, pin, number, reference, nif, name, lastname, birth,
+		        sub_type, address, postal_code, local, country, ayuntamiento, zone, email,
+		        phone, mobile, soc_type, observation7, observation5, observation10,
+		        observation11, observation19, sub_permit, observation_person, date_register, roles
+		    )
+		    SELECT
+		        user_id,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'identify' THEN meta_value END) as identify,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'pin' THEN meta_value END) as pin,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'number' THEN meta_value END) as number,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'reference' THEN meta_value END) as reference,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'nif' THEN meta_value END) as nif,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'first_name' THEN meta_value END) as name,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'lastname' THEN meta_value END) as lastname,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'birth' THEN meta_value END) as birth,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'sub_type' THEN meta_value END) as sub_type,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'address' THEN meta_value END) as address,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'postal_code' THEN meta_value END) as postal_code,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'local' THEN meta_value END) as local,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'country' THEN meta_value END) as country,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'ayuntamiento' THEN meta_value END) as ayuntamiento,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'zone' THEN meta_value END) as zone,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'email' THEN meta_value END) as email,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'phone' THEN meta_value END) as phone,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'mobile' THEN meta_value END) as mobile,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'soc_type' THEN meta_value END) as soc_type,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'observation7' THEN meta_value END) as observation7,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'observation5' THEN meta_value END) as observation5,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'observation10' THEN meta_value END) as observation10,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'observation11' THEN meta_value END) as observation11,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'observation19' THEN meta_value END) as observation19,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'sub_permit' THEN meta_value END) as sub_permit,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'observation_person' THEN meta_value END) as observation_person,
+		        COALESCE(NULLIF(GROUP_CONCAT(CASE WHEN meta_key = 'date_register' THEN meta_value END), ''), NULL) as date_register,
+		        GROUP_CONCAT(CASE WHEN meta_key = 'roles' THEN meta_value END) as roles
+		    FROM
+		        $this->table_meta
+		    WHERE
+		        meta_key IN (
+		            'identify', 'pin', 'number', 'reference', 'nif', 'first_name', 'lastname', 'birth',
+		            'sub_type', 'address', 'postal_code', 'local', 'country', 'ayuntamiento', 'zone',
+		            'email', 'phone', 'mobile', 'soc_type', 'observation7', 'observation5',
+		            'observation10', 'observation11', 'observation19', 'sub_permit', 'observation_person',
+		            'date_register', 'roles'
+		        )
+		    GROUP BY user_id
+		    HAVING MAX(CASE WHEN meta_key = 'number' THEN meta_value END) <> ''";
 
 		$this->wpdb->query( $sql );
 	}
